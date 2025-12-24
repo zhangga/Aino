368b97ab697322cf2c0a97ccd93ebbe3419afd34
zhangzeqiang.game <383523842@qq.com>
2025-12-24 18:10:45 +0800

    增加扩展

---
+++ aino_ext/components/embedding/common/embedding.go
@@ -0,0 +1,107 @@
+package common
+
+import (
+	"context"
+	"fmt"
+	"net/http"
+	"time"
+
+	"github.com/bytedance/gopkg/util/logger"
+	"github.com/bytedance/sonic"
+	"github.com/cloudwego/eino/components/embedding"
+	"github.com/zhangga/aino/conf"
+	"github.com/zhangga/aino/pkg/httppkg"
+)
+
+type EmbeddingImpl struct {
+	config *EmbeddingConfig
+}
+
+type EmbeddingConfig struct {
+	BaseURL string
+	APIKey  string
+	Model   string
+}
+
+type EmbeddingRequest struct {
+	Model string      `json:"model"`
+	Input []InputItem `json:"input"`
+}
+
+type InputItem struct {
+	Type     string    `json:"type"`
+	Text     string    `json:"text,omitempty"`
+	ImageURL *ImageURL `json:"image_url,omitempty"`
+}
+
+type ImageURL struct {
+	URL string `json:"url"`
+}
+
+type EmbeddingResponse struct {
+	Created int64 `json:"created"`
+	Data    struct {
+		Object    string    `json:"object"`
+		Embedding []float64 `json:"embedding"`
+	} `json:"data"`
+	Id     string `json:"id"`
+	Model  string `json:"model"`
+	Object string `json:"object"`
+	Usage  struct {
+		PromptTokens        int            `json:"prompt_tokens"`
+		TotalTokens         int            `json:"total_tokens"`
+		PromptTokensDetails map[string]int `json:"prompt_tokens_details"`
+	} `json:"usage"`
+}
+
+func newEmbedding(ctx context.Context) (emb embedding.Embedder, err error) {
+	// TODO Modify component configuration here.
+	config := &EmbeddingConfig{
+		BaseURL: conf.GlobalConfig.EmbedConfig.BaseURL,
+		APIKey:  conf.GlobalConfig.EmbedConfig.APIKey,
+		Model:   conf.GlobalConfig.EmbedConfig.Model,
+	}
+	emb = &EmbeddingImpl{config: config}
+	return emb, nil
+}
+
+func (impl *EmbeddingImpl) EmbedStrings(ctx context.Context, texts []string, opts ...embedding.Option) ([][]float64, error) {
+	client := httppkg.NewDefaultClient(15 * time.Second)
+	client.SetHeader("Authorization", "Bearer "+impl.config.APIKey)
+	var inputs []InputItem
+	for _, text := range texts {
+		inputs = append(inputs, InputItem{
+			Type: "text",
+			Text: text,
+		})
+	}
+	req := &httppkg.Request{
+		Method: "POST",
+		URL:    impl.config.BaseURL,
+		Body: EmbeddingRequest{
+			Model: impl.config.Model,
+			Input: inputs,
+		},
+	}
+
+	resp, err := client.Do(ctx, req)
+	if err != nil {
+		logger.Errorf("failed to get embedding: %v", err)
+		return nil, err
+	}
+
+	if resp.StatusCode != http.StatusOK {
+		logger.Errorf("failed to get embedding, status code: %d", resp.StatusCode)
+		return nil, fmt.Errorf("failed to get embedding, status code: %d", resp.StatusCode)
+	}
+
+	var embeddingResp EmbeddingResponse
+	if err = sonic.Unmarshal(resp.Body, &embeddingResp); err != nil {
+		logger.Errorf("failed to unmarshal embedding response: %v", err)
+		return nil, err
+	}
+
+	var results [][]float64
+	results = append(results, embeddingResp.Data.Embedding)
+	return results, nil
+}
---
+++ aino_ext/components/embedding/common/go.mod
@@ -0,0 +1,40 @@
+module github.com/zhangga/aino/aino_ext/components/embedding/common
+
+go 1.25.5
+
+require (
+	github.com/bytedance/gopkg v0.1.3
+	github.com/bytedance/sonic v1.14.2
+	github.com/cloudwego/eino v0.7.12
+	github.com/zhangga/aino v0.0.0-20251224083251-77a32911e620
+)
+
+require (
+	github.com/bahlo/generic-list-go v0.2.0 // indirect
+	github.com/buger/jsonparser v1.1.1 // indirect
+	github.com/bytedance/sonic/loader v0.4.0 // indirect
+	github.com/cloudwego/base64x v0.1.6 // indirect
+	github.com/dustin/go-humanize v1.0.1 // indirect
+	github.com/eino-contrib/jsonschema v1.0.3 // indirect
+	github.com/goph/emperror v0.17.2 // indirect
+	github.com/json-iterator/go v1.1.12 // indirect
+	github.com/klauspost/cpuid/v2 v2.2.9 // indirect
+	github.com/mailru/easyjson v0.7.7 // indirect
+	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
+	github.com/modern-go/reflect2 v1.0.2 // indirect
+	github.com/nikolalohinski/gonja v1.5.3 // indirect
+	github.com/pelletier/go-toml/v2 v2.2.4 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
+	github.com/sirupsen/logrus v1.9.3 // indirect
+	github.com/slongfield/pyfmt v0.0.0-20220222012616-ea85ff4c361f // indirect
+	github.com/twitchyliquid64/golang-asm v0.15.1 // indirect
+	github.com/wk8/go-ordered-map/v2 v2.1.8 // indirect
+	github.com/yargevad/filepathx v1.0.0 // indirect
+	go.uber.org/multierr v1.10.0 // indirect
+	go.uber.org/zap v1.27.1 // indirect
+	golang.org/x/arch v0.11.0 // indirect
+	golang.org/x/exp v0.0.0-20230905200255-921286631fa9 // indirect
+	golang.org/x/sys v0.39.0 // indirect
+	gopkg.in/natefinch/lumberjack.v2 v2.2.1 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
+)
